297c3555a0eb56d2a87e1413dbfa4c1659f3f346
jakedamico <jakedamico@gmail.com>
2022-06-09 15:13:43 -0400

    profile image added to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                p={3}
+                mb={6}
+                align="center"
+            >
                 Hello, I&apos;m a Computer Science student from Cincinnati!
             </Box>
 
@@ -13,6 +19,17 @@ const Page = () => {
                         Jake D'Amico
                     </Heading>
                     <p>Digital Craftsman ( Developer / Designer )</p>
+                </Box>
+                <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+                    <Image
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        maxWidth="150px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="/images/jake.jpeg"
+                        alt="Profile Image" />
                 </Box>
             </Box>
         </Container>
